26b10b7e871de3acb4dc45269468fe3d68eded9e
atr0phy <atrophyworks@gmail.com>
2019-09-14 08:06:06 +0900

    Update

--- Chapter12/Practice.hs
+++ Chapter12/Practice.hs
@@ -26,3 +26,73 @@ instance Functor Tree where
 -}
 
 -- 4
+
+newtype ZipList a = Z [a] deriving Show
+
+instance Functor ZipList where
+  -- fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap g (Z xs) = Z (fmap g xs)
+
+instance Applicative ZipList where
+  -- pure :: a -> ZipList a
+  -- (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+  pure x = Z (repeat x)
+  (Z gs) <*> (Z xs) = Z [ g x | (g, x) <- zip gs xs ]
+
+-- 6
+{-
+  instance Monad ((->) a) where
+    f >>= k = \x -> k (f x) x
+-}
+
+-- 7
+data Expr a = Var a | Val Int | Add (Expr a) (Expr a) deriving Show
+
+instance Functor Expr where
+  -- fmap :: Functor f => (a -> b) -> f a -> f b
+  -- fmap :: Functor f => (a -> b) -> Expr a -> Expr b
+  fmap g (Var a  ) = Var (g a)
+  fmap _ (Val n  ) = Val n
+  fmap g (Add x y) = Add (fmap g x) (fmap g y)
+
+instance Applicative Expr where
+  -- pure :: Applicative a => a -> Expr a
+  -- <*> :: Expr (a -> b) -> Expr a -> Expr b
+  pure = Var
+  Var g <*> ex = fmap g ex
+
+instance Monad Expr where
+  -- (>>=) :: Monad f => f a -> (a -> f b) -> f b
+  Var x   >>= g = g x
+  Val x   >>= _ = Val x
+  Add x y >>= g = Add (x >>= g) (y >>= g)
+
+-- 8
+type State = Int
+
+-- type ST = State -> State
+-- type ST a = State -> (a, State)
+newtype ST a = S (State -> (a, State))
+
+instance Functor ST where
+  -- fmap :: (a -> b) -> ST a -> ST b
+  fmap g st = do
+    s <- st
+    return $ g s
+
+instance Applicative ST where
+  -- pure :: a -> ST a
+  pure x = S (\s -> (x, s))
+
+-- (<*>) :: ST (a -> b) -> ST a -> ST b
+  stf <*> stx = do
+    f <- stf
+    x <- stx
+    return $ f x
+
+instance Monad ST where
+  -- (>>=) :: ST a -> (a -> ST b) -> ST b
+  st >>= f = S (\s -> let (x, s') = app st s in app (f x) s')
+
+app :: ST a -> State -> (a, State)
+app (S st) = st
